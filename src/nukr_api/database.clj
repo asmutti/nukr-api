f014e49fb69f575431eef063b64aa472060415c0
Alexandre Mutti <alexmutti@gmail.com>
2019-04-26 16:11:23 -0300

    completed add-friend method

--- src/nukr_api/database.clj
+++ src/nukr_api/database.clj
@@ -2,28 +2,39 @@
 
 
 ; consider def or defonce
-(def nukr
+(def nukr-database
   "social media database"
   (atom {
          :name "Nukr"
-         :accounts #{}
+         :accounts {}
          }))
 
 ;; all accounts are stored in an atom, consistency is guaranteed during a swap! within the atom.
-(defn create-account [cpf name birthday sex privacy]
+(defn create-account! [id name birthday sex privacy]
+  "Creates one account and commit it to the database."
    (let [account
-         {:cpf cpf
-          :name name
+         {:name name
           :birthday birthday
           :sex sex
           :privacy privacy
           :friends #{}}]
-     (swap! nukr update-in [:accounts] assoc cpf account)))
+     (swap! nukr-database update-in [:accounts] assoc id account)))
 
+(defn operation [id-origin id-dest]
+  "updates the database with the new friend id"
+  (fn [database]
+    (update-in database [:accounts id-origin :friends] conj id-dest)))
 
-
-
-
+;this method returns a new database copy after the new connection between friends are
+;successfully made
+(defn transact! [database operations]
+  (swap! database
+         (fn [database]
+           (reduce
+             (fn [curr-database operation]
+               (operation curr-database))
+             database
+             operations))))
 
 (comment
 
@@ -31,7 +42,7 @@
     "for now, accounts are 5 random numbers"
     (atom {
            :name "Nukr"
-           :accounts #{
+           :accounts {
                        12345 {
                               :name "Alexandre Mutti"
                               :birthday "20/04/1992"
@@ -56,4 +67,19 @@
          :privacy false
          :friends #{}}]
     (swap! example update-in [:accounts] assoc 12090 account))
+
+  {:get-account [(get-in @example [:accounts 12094])]}
+
+  {:get-account [(get-in @example [:accounts 12345 :sex])]}
+
+
+  (create-account 12314 "Loki Odinson" "10/02/300" "Male" true)
+  (create-account 12313 "Thor Odinson" "10/02/300" "Male" false)
+  (create-account 12354 "Odin Borson" "10/02/-1021" "Male" false)
+
+  ;;atualiza a key :friends dentro de :accounts
+  (swap! example update-in [:accounts 12345 :friends] conj 12094)
+
+  (transact! example [(operation 12345 12354) (operation 12354 12345)])
+
   )
