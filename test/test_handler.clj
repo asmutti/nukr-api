69f7e0b418e27c0eb11870d2943e9efc0e158362
Alexandre Mutti <alexmutti@gmail.com>
2019-05-03 17:49:07 -0300

    added more tests

--- test/test_handler.clj
+++ test/test_handler.clj
@@ -10,8 +10,18 @@
 
 (deftest http-test
 
-  (testing "Test GET request to /api/adduser/:id returns expected response"
+  (testing "Test GET request to /api/:id returns expected response"
     (let [response (app (-> (mock/request :get  "/api/user/12345")))
+          body     (parse-body (:body response))]
+      (is (= (:status response) 200))))
+
+  (testing "Test GET request to /api/user/12345/friend-suggestion returns expected response"
+    (let [response (app (-> (mock/request :get  "/api/user/12345/friend-suggestion")))
+          body     (parse-body (:body response))]
+      (is (= (:status response) 200))))
+
+  (testing "Test POST request to /api/user/:id/add-friend/:fid returns expected response"
+    (let [response (app (-> (mock/request :post  "/api/user/12342/add-friend/12347")))
           body     (parse-body (:body response))]
       (is (= (:status response) 200))))
 
@@ -27,4 +37,19 @@
                                (mock/content-type "application/json")
                                (mock/body (cheshire/generate-string user))))
              body (parse-body (:body response))]
-         (is (= (:status response) 200)))))
+         (is (= (:status response) 200))))
+
+  (testing "Test POST request to /api/user/98989 with missing attribute returns expected response"
+    (let [user {:id       98989
+                :name     "Peter Parker"
+                :sex      "Male"
+                :privacy  false
+                :friends []
+                }
+          response (app (-> (mock/request :post "/api/user/98989")
+                            (mock/content-type "application/json")
+                            (mock/body (cheshire/generate-string user))))
+          body (parse-body (:body response))]
+      (is (= (:status response) 400))))
+
+  )
