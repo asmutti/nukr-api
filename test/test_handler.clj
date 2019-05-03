1d238cd405c3e58a2794bdf24a6ec7230c171bce
Alexandre Mutti <alexmutti@gmail.com>
2019-05-03 09:58:02 -0300

    testing endpoints partial

--- test/test_handler.clj
+++ test/test_handler.clj
@@ -1,2 +1,37 @@
 (ns test-handler
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [cheshire.core :as cheshire]
+            [nukr.handler :refer :all]))
+
+(defn parse-body [body]
+  (cheshire/parse-string (slurp body) true))
+
+
+(deftest a-test
+
+  (testing "Test GET request to /hello?name={a-name} returns expected response"
+    (let [response (app (-> (mock/request :get  "/api/user/12345")))
+          body     (parse-body (:body response))]
+      (is (= (:status response) 200))))
+
+
+  (testing "Test POST request to /echo returns expected response"
+       (let [user {:id       98989
+                   :name     "Peter Parker"
+                   :birthday "03/05/1998"
+                   :sex      "Male"
+                   :privacy  false
+                   :friends []
+                   }
+             response (app (-> (mock/request :post "/api/user/98989")
+                               (mock/content-type "application/json")
+                               (mock/body (cheshire/generate-string user))))
+             body (parse-body (:body response))]
+         (is (= (:status response) 200)))))
+)
+
+
+
+
+
